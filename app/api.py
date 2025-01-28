fd7fcf5aecec4026d596cf2486a421b1c9586d36
nathalie sylla <nathaliesylla@MBP-de-nathalie.lan>
2025-01-28 22:31:22 +0100

    model chargé directement

--- app/api.py
+++ app/api.py
@@ -6,10 +6,9 @@ import joblib
 
 # Charger le modèle enregistré
 model_path = "model_pipeline/model.pkl"  # Chemin relatif vers le fichier
-model_data = joblib.load(model_path)  # Charger le fichier .plk
+model = joblib.load(model_path)  # Le modèle est directement chargé, pas besoin d'une clé "model"
 
-# Extraire le modèle et le seuil
-model = model_data["model"]  # Modèle entraîné
+# le seuil
 optimal_threshold = 0.55  # Valeur fixe du seuil
 
 # Initialisation de FastAPI
@@ -21,20 +20,15 @@ class ClientData(BaseModel):
 
 @app.post("/predict/")
 async def predict(client_data: ClientData):
-    """
-    Prédire la probabilité de défaut et la classe basée sur un seuil optimal.
-    Args:
-        - client_data: Caractéristiques du client.
-    Returns:
-        - Probabilité de défaut
-        - Classe prédite
-    """
-    features = np.array(client_data.features).reshape(1, -1)  # Mise en forme des données
-    y_prob = model.predict_proba(features)[0][1]  # Probabilité de défaut
-    decision = "Refusé" if y_prob >= optimal_threshold else "Accepté"  # Décision basée sur le seuil
+    try:
+        features = np.array(client_data.features).reshape(1, -1)  # Mise en forme des données
+        y_prob = model.predict_proba(features)[0][1]  # Probabilité de défaut
+        decision = "Refusé" if y_prob >= optimal_threshold else "Accepté"  # Décision basée sur le seuil
 
-    return {
-        "threshold": optimal_threshold,
-        "probability": round(y_prob, 4),
-        "class": decision
-    }
+        return {
+            "threshold": optimal_threshold,
+            "probability": round(y_prob, 4),
+            "class": decision
+        }
+    except Exception as e:
+        return {"error": str(e)}
